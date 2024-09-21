1dff6aea3faf5eb197f6f0ffdeb97300afdcb57d
PageotD <damien.pageot@gmail.com>
2024-09-21 10:25:45 +0200

    style: improve readability in test_commands.py

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,7 +1,7 @@
 import unittest
 import argparse
 from runmd.commands import create_commons, add_run_command, add_show_command, add_list_command, add_hist_command, add_vault_command
-from runmd.commands import RUNCMD, SHOWCMD, LISTCMD, HISTCMD
+from runmd.commands import RUNCMD, SHOWCMD, LISTCMD, HISTCMD, VAULTCMD
 
 class TestAddCommands(unittest.TestCase):
 
@@ -10,6 +10,10 @@ class TestAddCommands(unittest.TestCase):
         self.parser = argparse.ArgumentParser()
         self.common_parser = create_commons()
         self.subparsers = self.parser.add_subparsers(dest="command")
+
+    # --------------------------------------------------
+    # >> ADD_RUN_COMMAND
+    # --------------------------------------------------
 
     def test_add_run_command(self):
         """Test if 'run' command is correctly added."""
@@ -20,12 +24,20 @@ class TestAddCommands(unittest.TestCase):
         self.assertEqual(args.tag, None)
         self.assertEqual(args.env, [])
 
+    # --------------------------------------------------
+    # >> ADD_SHOW_COMMAND
+    # --------------------------------------------------
+
     def test_add_show_command(self):
         """Test if 'show' command is correctly added."""
         add_show_command(self.subparsers, self.common_parser)
         args = self.parser.parse_args(['show', 'my_block'])
         self.assertEqual(args.command, SHOWCMD)
         self.assertEqual(args.blockname, 'my_block')
+
+    # --------------------------------------------------
+    # >> ADD_LIST_COMMAND
+    # --------------------------------------------------
 
     def test_add_list_command(self):
         """Test if 'list' command is correctly added."""
@@ -40,6 +52,10 @@ class TestAddCommands(unittest.TestCase):
         args = self.parser.parse_args(['list', '--tag', 'my_tag'])
         self.assertEqual(args.command, LISTCMD)
         self.assertEqual(args.tag, 'my_tag')
+
+    # --------------------------------------------------
+    # >> ADD_HIST_COMMAND
+    # --------------------------------------------------
 
     def test_add_hist_command(self):
         """Test if 'hist' command is correctly added."""
@@ -56,27 +72,31 @@ class TestAddCommands(unittest.TestCase):
         self.assertEqual(args.command, HISTCMD)
         self.assertTrue(args.clear)
 
-    def test_vault_command(self):
+    # --------------------------------------------------
+    # >> ADD_VAULT_COMMAND
+    # --------------------------------------------------
+
+    def test_add_vault_command(self):
         """Test if 'vault' command is correctly added."""
         add_vault_command(self.subparsers)
         args = self.parser.parse_args(['vault'])
-        self.assertEqual(args.command, 'vault')
+        self.assertEqual(args.command, VAULTCMD)
 
-    def test_vault_command_with_encrypt(self):
+    def test_add_vault_command_with_encrypt(self):
         """Test if 'vault' command parses the --encrypt option."""
         add_vault_command(self.subparsers)
         args = self.parser.parse_args(['vault', '--encrypt', 'test.md'])
-        self.assertEqual(args.command, 'vault')
+        self.assertEqual(args.command, VAULTCMD)
         self.assertEqual(args.encrypt, ['test.md'])
 
-    def test_vault_command_with_decrypt(self):
+    def test_add_vault_command_with_decrypt(self):
         """Test if 'vault' command parses the --decrypt option."""
         add_vault_command(self.subparsers)
         args = self.parser.parse_args(['vault', '--decrypt', 'test.md'])
-        self.assertEqual(args.command, 'vault')
+        self.assertEqual(args.command, VAULTCMD)
         self.assertEqual(args.decrypt, ['test.md'])
 
-    def test_vault_command_mutual_exclusion(self):
+    def test_add_vault_command_mutual_exclusion(self):
         """Test if 'vault' command does not allow both --encrypt and --decrypt options."""
         add_vault_command(self.subparsers)
         with self.assertRaises(SystemExit):
